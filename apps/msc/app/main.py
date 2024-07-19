34b15bdc2f60be9ffd309252c18aaf8674101d3d
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-07-19 23:05:28 +0300

    create basic fastapi asgi web app for msc app

--- apps/msc/app/main.py
+++ apps/msc/app/main.py
@@ -1,9 +1,17 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
 from libs.commons.lib import print_hello_app
 
+app = FastAPI(title="MSC Supplier Gateway")
 
-def main() -> None:
+
+@app.get("/", include_in_schema=False)
+def index() -> RedirectResponse:
     print_hello_app("MSC")
+    return RedirectResponse("/docs")
 
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run("apps.msc.app.main:app", host="127.0.0.1", port=8000, reload=True)
