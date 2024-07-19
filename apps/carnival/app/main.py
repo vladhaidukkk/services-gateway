7c4ed86373a7084d1bd48dfb9ee0d94bf28c24f0
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-07-19 23:21:19 +0300

    create other supplier app skeletons

--- apps/carnival/app/main.py
+++ apps/carnival/app/main.py
@@ -4,14 +4,14 @@ from fastapi.responses import RedirectResponse
 
 from libs.commons.lib import print_hello_app
 
-app = FastAPI(title="MSC Supplier Gateway")
+app = FastAPI(title="Carnival Supplier Gateway")
 
 
 @app.get("/", include_in_schema=False)
 def index() -> RedirectResponse:
-    print_hello_app("MSC")
+    print_hello_app("Carnival")
     return RedirectResponse("/docs")
 
 
 if __name__ == "__main__":
-    uvicorn.run("apps.msc.app.main:app", host="127.0.0.1", port=8002, reload=True)
+    uvicorn.run("apps.carnival.app.main:app", host="127.0.0.1", port=8001, reload=True)
--- apps/royal/app/main.py
+++ apps/royal/app/main.py
@@ -4,14 +4,14 @@ from fastapi.responses import RedirectResponse
 
 from libs.commons.lib import print_hello_app
 
-app = FastAPI(title="MSC Supplier Gateway")
+app = FastAPI(title="Royal Caribbean Supplier Gateway")
 
 
 @app.get("/", include_in_schema=False)
 def index() -> RedirectResponse:
-    print_hello_app("MSC")
+    print_hello_app("Royal Caribbean")
     return RedirectResponse("/docs")
 
 
 if __name__ == "__main__":
-    uvicorn.run("apps.msc.app.main:app", host="127.0.0.1", port=8002, reload=True)
+    uvicorn.run("apps.royal.app.main:app", host="127.0.0.1", port=8004, reload=True)
